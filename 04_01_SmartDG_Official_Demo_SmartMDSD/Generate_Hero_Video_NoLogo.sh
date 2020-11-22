a7da9af37a25e664139ea1eaeef3ef7da942dabc
nagrath-hsu <vineet.nagrath@gmail.com>
2020-11-22 14:30:28 +0100

    Added scripts to generate Hero videos

--- 04_01_SmartDG_Official_Demo_SmartMDSD/Generate_Hero_Video_NoLogo.sh
+++ 04_01_SmartDG_Official_Demo_SmartMDSD/Generate_Hero_Video_NoLogo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-VIDEO_FILE="ShortcutProject/ShortcutProject.mp4"
+VIDEO_FILE="ShortcutProject/ShortcutProject_NoLogo.mp4"
 SUBTITLES_FILE="GnomeSubtitlesEditorFile/Commentary.srt"
-HERO_FILE_NAME="SmartDG_Official_Demo_SmartMDSD.mp4"
+HERO_FILE_NAME="SmartDG_Official_Demo_SmartMDSD_NoLogo.mp4"
 echo "Burning $SUBTITLES_FILE to $VIDEO_FILE"
 echo "Style: ${SUBTITLE_STYLE}"
 echo "Output -------> $HERO_FILE_NAME"

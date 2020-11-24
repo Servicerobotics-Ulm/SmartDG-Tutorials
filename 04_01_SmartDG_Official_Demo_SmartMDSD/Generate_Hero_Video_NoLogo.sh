b1c3abc2d54de2d31488734c192252fb256a8a35
nagrath-hsu <vineet.nagrath@gmail.com>
2020-11-24 16:46:24 +0100

    Update README.md

--- 04_01_SmartDG_Official_Demo_SmartMDSD/Generate_Hero_Video_NoLogo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VIDEO_FILE="ShortcutProject/ShortcutProject_NoLogo.mp4"
-SUBTITLES_FILE="GnomeSubtitlesEditorFile/Commentary.srt"
-HERO_FILE_NAME="SmartDG_Official_Demo_SmartMDSD_NoLogo.mp4"
-echo "Burning $SUBTITLES_FILE to $VIDEO_FILE"
-echo "Style: ${SUBTITLE_STYLE}"
-echo "Output -------> $HERO_FILE_NAME"
-ffmpeg -i ${VIDEO_FILE} -vf subtitles=${SUBTITLES_FILE}:force_style='Fontname=DejaVu Serif' ${HERO_FILE_NAME}
